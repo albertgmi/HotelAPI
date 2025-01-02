8ecd9010d2db5364ea6e1901880f05d17272439d
albertgmi <gmitrzak.albert@gmail.com>
2025-01-02 15:41:58 +0100

    Services finished

---
+++ HotelAPI/Services/UserServiceFolder/IUserContextService.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace HotelAPI.Services.UserServiceFolder
+{
+    public interface IUserContextService
+    {
+        ClaimsPrincipal User { get; }
+        int? GetUserId { get; }
+    }
+}
