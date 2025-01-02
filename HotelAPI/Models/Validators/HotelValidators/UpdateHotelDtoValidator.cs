4f155020484afb69a8b96af6ab56088212f3b276
albertgmi <gmitrzak.albert@gmail.com>
2025-01-02 15:22:19 +0100

    Models and validators finished

---
+++ HotelAPI/Models/Validators/HotelValidators/UpdateHotelDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using HotelAPI.Models.HotelModels;
+
+namespace HotelAPI.Models.Validators.HotelValidators
+{
+    public class UpdateHotelDtoValidator : AbstractValidator<UpdateHotelDto>
+    {
+        public UpdateHotelDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description is required.");
+            RuleFor(x => x.ContactNumber)
+                .NotEmpty().WithMessage("Contact number is required.")
+                .Matches(@"^\d{3}-\d{3}-\d{3}$").WithMessage("Phone number must by in this pattern: ###-###-###.");
+        }
+    }
+}
---
+++ HotelAPI/Models/Validators/RoomValidators/UpdateRoomDtoValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using HotelAPI.Models.RoomModels;
+
+namespace HotelAPI.Models.Validators.RoomValidators
+{
+    public class UpdateRoomDtoValidator : AbstractValidator<UpdateRoomDto>
+    {
+        private readonly string[] roomTypes = new string[]
+        {
+            "Single", "Double", "Twin", "Triple", "Quad", "Suite",
+            "King", "Queen", "Family", "Connecting", "Accessible",
+            "Penthouse", "Dormitory"
+        };
+        public UpdateRoomDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description is required.")
+                .MinimumLength(30).WithMessage("Description must be atleast 30 characters long");
+
+            RuleFor(room => room.Type)
+                .NotEmpty().WithMessage("Room type is required.")
+                .Must(type => roomTypes.Contains(type))
+                .WithMessage("Room type must be one of the following: " + string.Join(", ", roomTypes));
+
+            RuleFor(x => x.PricePerNight)
+                .GreaterThan(0).WithMessage("Price per night must be greater than 0.");
+        }
+    }
+}
